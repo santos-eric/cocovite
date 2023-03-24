c260209510334aed8d3e9f09c1d230bc2ba26295
santos-eric <esantosdev77@gmail.com>
2023-03-24 11:16:09 -0600

    redesigned and completed footer on root to display on every page

---
+++ src/routes/footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../styles/footer.sass";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
+import { SiTiktok } from "react-icons/si";
+
+const footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="title">
+          <h1>Follow Us</h1>
+        </div>
+        <div className="icons">
+          <a
+            href="https://www.tiktok.com/@coco.lokoep?lang=en"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <SiTiktok size="30" className="icon" />
+          </a>
+
+          <a
+            href="https://www.facebook.com/CocolokoElPaso/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaFacebookSquare size="30" className="icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/coco.lokoep/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaInstagram size="30" className="icon" />
+          </a>
+        </div>
+      </footer>
+      <div className="copyright">
+        <h4>
+          &copy; Copyright 2021 | Crafted By
+          <a
+            href="https://www.linkedin.com/in/ericsantos01/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            -Eric Santos
+          </a>
+        </h4>
+      </div>
+    </>
+  );
+};
+export default footer;
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,16 +1,16 @@
 // place outlet where you want the children route to display
 import { Outlet, Link } from 'react-router-dom'
 import Navbar from './Navbar'
+import Footer from './footer'
 
 export default function Root() {
   return (
     <>
-      <div id="sidebar">
-        <Navbar/>
-       </div>
+      <Navbar />
       <div id="detail">
         <Outlet/>
       </div>
+      <Footer/>
     </>
   );
 }
